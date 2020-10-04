ebfc16207bc9df8870ab62a0ba6c04a06e8896c1
JQwebsite <71882072+JQwebsite@users.noreply.github.com>
2020-10-04 22:53:13 +0800

    .

--- js/script.js
+++ js/script.js
@@ -1,18 +1,12 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-    
+function clicked(){
+  dropdown=document.getElementById('dropdown').style
+  navLinks=document.getElementsByTagName('nav')[0].style
+  if (dropdown.transform=="rotateX(180deg)") {
+    dropdown.transform="rotateX(0deg)"
+    navLinks.display="none"
   } else {
-    navbar.classList.remove("sticky");
+    dropdown.transform="rotateX(180deg)"
+    navLinks.display="block"
   }
+
 }
