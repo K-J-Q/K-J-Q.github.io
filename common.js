bd289d5cb22fd03a8583cbeda146bd63987dd020
JQ <jianquan08@gmail.com>
2022-12-25 23:07:42 +0800

    test

--- common.js
+++ common.js
@@ -1,7 +1,8 @@
+document.getElementById("footerLink").innerHTML = "The full URL of this page is:<br>" + window.location.href;
+
 $(function () {
     $("body").prepend("<div id='header'></div>");
     $("body").append("<div id='footer'></div>");
     $("#header").load("header.html");
     $("#footer").load("footer.html");
-    console.log("test");
 });
